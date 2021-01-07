d91343cc47ac10fb9769d0bfdb43bbee829e843d
Arya Rifqi Pratama <arya.rifqi@alodokter.com>
2021-01-07 18:39:37 +0700

    init API

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,30 +1,26 @@
-class UsersController < ApplicationController
+module Api
+  class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
 
-  # GET /users
   def index
     @users = User.all
 
     render json: @users
   end
 
-  # GET /users/1
   def show
     render json: @user
   end
 
-  # POST /users
   def create
     @user = User.new(user_params)
-
     if @user.save
-      render json: @user, status: :created, location: @user
+      render json: @user, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /users/1
   def update
     if @user.update(user_params)
       render json: @user
@@ -33,9 +29,21 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
   def destroy
-    @user.destroy
+    if @user.destroy
+      render json: {message: "data has been deleted"}, status: 	:ok
+    else
+      render status: :unprocessable_entity
+    end
+  end
+
+  def login
+    @user = User.find_by(email: user_params[:email])
+    if BCrypt::Password.new(@user.password) == user_params[:password]
+      render json: {message: "login success"}
+    else
+      render status: :unprocessable_entity
+    end
   end
 
   private
@@ -46,6 +54,8 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:email, :passowrd, :fullname, :birthday, :city)
+      params.require(:user).permit(:email, :password, :fullname, :birthday, :city)
     end
+
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,24 @@
+require 'bcrypt'
+
 class User
   include Mongoid::Document
   include Mongoid::Timestamps
+  include BCrypt
+
+  before_save :encrypt_password
+
+  validates :email, presence: true, uniqueness: true
+  validates :fullname, :birthday, :city, presence: true
+  validates :password, presence: true, length: { minimum: 8, maximum: 16 }
+
   field :email, type: String
-  field :passowrd, type: String
+  field :password, type: String
   field :fullname, type: String
   field :birthday, type: Date
   field :city, type: String
+
+  def encrypt_password
+    self.password = BCrypt::Password.create(password) 
+  end
+
 end
